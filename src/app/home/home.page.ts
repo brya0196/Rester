6623c2c05a697d7db1233909c7204f0f3606476a
Bryan Astacio <bastacio@Bryans-MacBook-Pro.local>
2019-04-02 18:41:20 -0400

    added the stop timer action with the button to start or stop

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -30,7 +30,7 @@ export class HomePage {
       clearInterval(this.timer);
     }
 
-    if (this.overallTimer) {
+    if (!this.overallTimer) {
       this.progressTimer();
     }
 
@@ -54,7 +54,40 @@ export class HomePage {
   }
 
   progressTimer() {
-    
+    let countDownDate = new Date();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+    }, 1000);
+  }
+
+  pad(num, size) {
+    let s = num + '';
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    clearInterval(this.overallTimer);
+    this.overallTimer = false;
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+    this.elapsed = {
+      h: '00',
+      m: '00',
+      s: '00'
+    }
   }
 
 }
